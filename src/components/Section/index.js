0ae8b4c09003725b2983fa955aa3f0db7881e415
lionel-kg <lionel77350@gmail.com>
2023-12-15 16:33:32 +0100

    fix card

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import styles from './index.module.scss';
 import Card from '../Card/index';
 import PageTitle from '../PageTitle/index';
 
-const Index = ({ title, cards, CardComponent }) => {
+const Index = ({title, cards, CardComponent}) => {
   const [showAll, setShowAll] = useState(false);
-  const [showLess, setShowLess] = useState(false);
-  const displayedSongs = showAll ? cards : cards.slice(0, 4);
 
-  const handleShowAllClick = () => {
-    setShowAll(true);
-    setShowLess(true);
-  };
-
-  const handleShowLessClick = () => {
-    setShowAll(false);
-    setShowLess(false);
+  const handleToggleShow = () => {
+    setShowAll(!showAll);
   };
 
   return (
     <div className={styles.container_section}>
       <div className={styles.section_title}>
         <h2>{title}</h2>
-        {!showAll && (
-          <div onClick={handleShowAllClick} className={styles.show_button}>
-            <p>Show all</p>
-          </div>
-        )}
-        {showLess && (
-          <div onClick={handleShowLessClick} className={styles.show_button}>
-            <p>Show less</p>
-          </div>
-        )}
+        <div onClick={handleToggleShow} className={styles.show_button}>
+          <p>{showAll ? 'Show less' : 'Show all'}</p>
+        </div>
       </div>
       <div className={styles.container_card}>
-        {cards.map((card, index) => (
+        {cards.slice(0, showAll ? cards.length : 4).map((card, index) => (
           <Card key={index} {...card} />
         ))}
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import PageTitle from '../components/PageTitle';
 import Section from '../components/Section/index';
-import { getAudios } from '@/services/audio.service';
-import Card from "@/components/Card";
+import {getAudios} from '@/services/audio.service';
+import Card from '@/components/Card';
 export default function Home() {
   const [audios, setAudios] = useState({});
   const [loading, setLoading] = useState(true);
